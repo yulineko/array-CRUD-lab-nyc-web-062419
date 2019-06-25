49c2d44864799f21f002be10904843bec6df70cf
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-25 02:40:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first()
 end
 
 def retrieve_last_element_from_array(array)
